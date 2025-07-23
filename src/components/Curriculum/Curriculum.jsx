b76a54a1366d36faf17be87b1ac019f7ad45b736
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-07-23 18:49:15 +0200

    feat(Section): add Section composition

--- src/components/Curriculum/Curriculum.jsx
+++ src/components/Curriculum/Curriculum.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react';
 import { Header } from "../Header/Header";
+import { Section } from "../Section/Section";
 import "./Curriculum.css";
 
 export const Curriculum = ({ data }) => {
@@ -16,6 +17,20 @@ export const Curriculum = ({ data }) => {
 	return (
 		<div className="curriculum-container">
 			<Header info={personalInfo} />
+            <Section title={"Experiencia"}>
+
+            </Section>
+            <Section title={"Educación"}>
+
+            </Section>
+            <Section title={"Habilidades"}>
+
+            </Section>
+            <Section title={"Idiomas"}>
+
+            </Section>
+                
+            
 		</div>
 	);
 };
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react';
+import './Section.css';
+
+export const Section = ({title}) => {
+
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
