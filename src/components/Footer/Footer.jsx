6e85fec573bb9b48895588cb741c6de1c3a67e6e
Juan Simón Abad <101209674+juansim87@users.noreply.github.com>
2025-08-04 19:43:09 +0200

    Revert "Selection Mode in Interactive CV"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,7 +31,7 @@ export const Footer = ({ cvData }) => {
 				</div>
 				<div className="footer-info">
 					<p>
-						📍<span>{location}</span> | 📧<span> {email}</span>
+						<span>📍{location}</span> | <span>📧 {email}</span>
 					</p>
 				</div>
 			</div>
