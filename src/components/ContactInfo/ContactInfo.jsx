0fc6969aca24f0169ac624a0bf9238015182225c
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-08-04 11:58:21 +0200

    chore(Header): Fix boxes in Contact Info

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -1,24 +1,34 @@
 // import { useState } from 'react';
 import "./ContactInfo.css";
-import githubLogo from "../../assets/icons/github-logo.png"
-import linkedinLogo from "../../assets/icons/linkedin-logo.png"
-import portfolioLogo from "../../assets/images/juan-simon-logo.png"
+import githubLogo from "../../assets/icons/github-logo.png";
+import linkedinLogo from "../../assets/icons/linkedin-logo.png";
+import portfolioLogo from "../../assets/images/juan-simon-logo.png";
 
 export const ContactInfo = ({ info }) => {
 	const { location, email, linkedin, github, portfolio } = info;
 
 	return (
 		<div className="contact-info">
-			<p>
-				📍 <span>{location}</span>
-			</p>
-			<p>
-				📧 <span>{email}</span>
-			</p>
+			<div className="location-and-mail">
+				<p>📍 {location}</p>
+				<p>📧 {email}</p>
+			</div>
 			<div className="rrss">
-				<div className="rrss-icon"><a href={github}><img src={githubLogo}/></a></div>
-				<div className="rrss-icon"><a href={linkedin}><img src={linkedinLogo}/></a></div>
-				<div className= "rrss-icon"><a href={portfolio}><img src={portfolioLogo}/></a></div>
+				<div className="rrss-icon">
+					<a href={github}>
+						<img src={githubLogo} />
+					</a>
+				</div>
+				<div className="rrss-icon">
+					<a href={linkedin}>
+						<img src={linkedinLogo} />
+					</a>
+				</div>
+				<div className="rrss-icon">
+					<a href={portfolio}>
+						<img src={portfolioLogo} />
+					</a>
+				</div>
 			</div>
 		</div>
 	);
