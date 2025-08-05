90c7ddf30d8291e6c15bf145c6d4e44230deaa14
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-08-05 15:45:54 +0200

    chore(ToggleModeButton): Add tootip for button hovering

--- src/components/ToggleModeButton/ToggleModeButton.jsx
+++ src/components/ToggleModeButton/ToggleModeButton.jsx
@@ -1,9 +1,18 @@
 // import { useState } from "react";
 import "./ToggleModeButton.css";
 
-export const ToggleModeButton = ({toggleMode, handleModeChange}) => {
+export const ToggleModeButton = ({ toggleMode, handleModeChange }) => {
 	return (
-		<div className="toggle-mode-button" role="button" onClick={() => handleModeChange()}>
+		<div
+			title={
+				toggleMode === "viewMode"
+					? "Activa la selección personalizada de secciones"
+					: "Vuelve al modo de visualización completo"
+			}
+			className="toggle-mode-button"
+			role="button"
+			onClick={() => handleModeChange()}
+		>
 			{toggleMode === "viewMode"
 				? "Cambiar a Modo Selección"
 				: "Cambiar a Modo Visualización"}
