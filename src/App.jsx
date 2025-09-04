af88ec99a973a6d22b0a63f99eb9ac808edd8740
LEGACY-OF-JUANC\Usuario <juansim87@gmail.com>
2025-09-04 11:34:09 +0200

    fix(App): fixed MainView

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,20 @@ import { MainView } from "./components/MainView/MainView";
 
 export const App = () => {
 	const [darkMode, setDarkMode] = useState(false);
-	const [showMainView,setShowMainView] = useState(false);
+	const [showMainView, setShowMainView] = useState(false);
+
 	const onToggleDarkMode = () => {
 		setDarkMode((prev) => !prev);
 	};
 
 	return (
 		<div className={darkMode ? "dark-mode" : "light-mode"}>
-			<div id="dark-mode-btn" role="button" onClick={onToggleDarkMode} aria-label="Cambiar entre modo oscuro y modo claro">
+			<div
+				id="dark-mode-btn"
+				role="button"
+				onClick={onToggleDarkMode}
+				aria-label="Cambiar entre modo oscuro y modo claro"
+			>
 				<img
 					src={darkMode ? sunIcon : moonIcon}
 					alt="Icono modo claro y modo oscuro"
